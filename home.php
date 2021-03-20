979ba3c8a1390d8bfbefc3eb65124d149fc4ee5f
princelylopes <54198477+princelylopes@users.noreply.github.com>
2021-03-20 08:08:45 +0530

    Update home.php

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <!-- Compiled and minified CSS -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
+  
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link rel="stylesheet" href="styles.css">
@@ -40,9 +40,9 @@
 </u>
 <?php
 $servername = "localhost";
-$username = "id16417071_princely";
-$password = "w-!<S24~FJ|^!h$";
-$db_name="id16417071_banking1";
+$username = "username";
+$password = "password";
+$db_name="database_name";
 
 $con = mysqli_connect($servername,$username,$password,$db_name);
 
